ab3f13a2125733e1dac0ac82e1fb11967901d02b
Konstantin Zhdanov <k.zhdan0v@yandex.ru>
2019-05-08 23:45:41 +0300

    Add implementation for the samples repository based on numbers.
    The repository can accept any subclasses of Number as its elements.
    Internally holds samples as Lists.

--- src/main/java/neuralnetwork/commons/samples/SamplesRepository.java
+++ src/main/java/neuralnetwork/commons/samples/SamplesRepository.java
@@ -1,9 +1,75 @@
 package neuralnetwork.commons.samples;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- *
+ * Repository for storing neural network's samples, containing numbers.
+ * @param <T> A numerical type that the samples hold.
  * @author Konstantin Zhdanov
  */
-public class SamplesRepository {
+public class SamplesRepository<T extends Number> {
+    private final List<String> header;
     
+    private final List<List<T>> items;
+    
+    public SamplesRepository() {
+        items = new ArrayList<>();
+        header = new LinkedList<>();
+    }
+    
+    public void add(List<T> sample) {
+        if (sample == null) {
+            throw new NullPointerException("Sample cannot be null");
+        }
+        items.add(sample);
+    }
+    
+    public void remove(int idx) {
+        items.remove(idx);
+    }
+    
+    public void setHeader(List<String> header) {
+        this.header.addAll(header);
+    }
+    
+    public List<String> getHeader() {
+        if (header.isEmpty() && !items.isEmpty()) {
+            createDefaultHeader();
+        }
+        return Collections.unmodifiableList(header);
+    }
+    
+    private void createDefaultHeader() {
+        header.clear();
+        int sampleSize = sampleSize();
+        for (int sampleVar = 0; sampleVar < sampleSize; sampleVar++) {
+            header.add(String.format("Var %d", sampleVar + 1));
+        }
+    }
+    
+    public List<T> getSample(int idx) {
+        return items.get(idx);
+    }
+    
+    public List<List<T>> getAll() {
+        return items;
+    }
+    
+    public int size() {
+        return items.size();
+    }
+    
+    public int sampleSize() {
+        if (items.isEmpty()) {
+            return 0;
+        }
+        return items.get(0).size();
+    }
+    
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
 }
